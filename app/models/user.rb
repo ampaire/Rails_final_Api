2976f46bf328259fe38d9c7a8018ebc34290c93c
ampaire <femayapr@gmail.com>
2020-09-17 15:09:24 -0600

    Add user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+  has_many :items
   has_secure_password
+  validates_presence_of :name, :email, :password, :image
+  validates_uniqueness_of :email
+  mount_uploader :image, ImageUploader
 end

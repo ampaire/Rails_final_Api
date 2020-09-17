6a82b203b7fd91d02b232360edb916780f8acb49
ampaire <femayapr@gmail.com>
2020-09-17 15:48:26 -0600

    Add private method for user params

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,15 +1,20 @@
 # app/controllers/authentication_controller.rb
 
 class AuthenticationController < ApplicationController
-  skip_before_action :authenticate_request
+  skip_before_action :authenticate_request, only :authenticate
 
   def authenticate
-    command = AuthenticateUser.call(params[:email], params[:password])
+    command = AuthenticateUser.new(auth_params[:email], auth_params[:password])
 
     if command.success?
       render json: { auth_token: command.result }
     else
       render json: { error: command.errors }, status: :unauthorized
     end
+  end
+  private
+
+  def auth_params
+    params.permit(:email, :password, :authentication)
   end
 end

e51ef33e84e7797cedc333f4a3404d5c661a2b22
ampaire <femayapr@gmail.com>
2020-09-17 15:03:42 -0600

    Fix the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :carts
   resources :items
-  post 'authenticate', to: 'authentication#authenticate'
+  resources :cart,except: %i[destroy]
+  post '/signup', to: 'users#create'
+  get '/profile', to: 'users#show'
+  put '/edit-profile', to: 'users#update'
+  post '/auth/login', to: 'authentication#authenticate'
+  delete '/cart', to: 'cart#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

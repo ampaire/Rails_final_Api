b90ef0345806bd03c1b2bd857542d9b792b3fd98
ampaire <femayapr@gmail.com>
2020-10-04 15:40:58 -0600

    Add changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, except: [:update, :create, :show]
   resources :items
-  resources :cart,except: %i[destroy]
+  resources :cart,except: %i[:destroy]
+  root :to => 'index#index'
   post '/signup', to: 'users#create'
   get '/profile', to: 'users#show'
   put '/edit-profile', to: 'users#update'

d458a37fcbf57cafabb786e65556c53dd2a72868
ampaire <femayapr@gmail.com>
2020-10-05 09:18:44 -0600

    Fix error with deployment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :favorites, dependent: :destroy
+  has_many :carts, dependent: :destroy
   has_many :items
   has_secure_password
   validates_presence_of :name, :email, :password, :image
